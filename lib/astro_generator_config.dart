89d262daeef9d9a164a3aabdc72230709e823d08
skutimechanic <koszowskimtb@gmail.com>
2023-01-26 21:53:01 +0100

    new config for api generation

--- lib/astro_generator_config.dart
+++ lib/astro_generator_config.dart
@@ -2,10 +2,10 @@ import 'package:openapi_generator_annotations/openapi_generator_annotations.dart
 
 @Openapi(
     additionalProperties:
-        DioAltProperties(pubName: 'astro_api', pubAuthor: 'skuti'),
+        DioProperties(pubName: 'astro_api', pubAuthor: 'arcsecond'),
     inputSpecFile: 'openapi_schema.yaml',
     generatorName: Generator.dio,
     alwaysRun: true,
     skipSpecValidation: true,
-    outputDirectory: 'lib/api/astro_api')
+    outputDirectory: 'api/astro_api')
 class AstroGeneratorConfig extends OpenapiGeneratorConfig {}
