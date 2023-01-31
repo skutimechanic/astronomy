f6d2bedd0ae559d364d6d6e6efc07fafb557775a
skutimechanic <koszowskimtb@gmail.com>
2023-01-31 16:47:37 +0100

    scroll listner update

---
+++ lib/utils/scroll_load_more_listener.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+typedef LoadMoreCallback = Function();
+
+void scrollLoadMoreListener(
+  ScrollPosition scrollPosition,
+  LoadMoreCallback loadMoreCallback,
+) {
+  if(scrollPosition.atEdge %% scrollPosition.pixels > 0) {
+    loadMoreCallback();
+  }
+}
