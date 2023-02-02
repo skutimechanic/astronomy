ee08844ea057bc4f1bdfc0474bd4f652a90cc98f
skutimechanic <koszowskimtb@gmail.com>
2023-02-02 19:48:24 +0100

    search ui part

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:astro_api/astro_api.dart';
 import 'package:astronom/repository/astro_repository.dart';
 import 'package:astronom/ui/home/widgets/activities_widget/activities_tab.dart';
 import 'package:astronom/ui/home/widgets/activities_widget/bloc/activities_bloc.dart';
@@ -10,8 +9,28 @@ import 'package:astronom/ui/widgets/tab_label.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _controller;
+  bool isSearchBarOpen = false;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,13 +38,14 @@ class HomePage extends StatelessWidget {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Astro'),
+          title: _titleBar(),
           bottom: const TabBar(
             tabs: [
               TabLabel(text: 'exoplanets'),
               TabLabel(text: 'activities'),
             ],
           ),
+          actions: _appBarActions(),
         ),
         backgroundColor: Colors.blueGrey,
         body: MultiBlocProvider(
@@ -48,5 +68,66 @@ class HomePage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  List<Widget>? _appBarActions() {
+    if (isSearchBarOpen) {
+      return null;
+    } else {
+      return [
+        IconButton(
+            onPressed: () => setState(() {
+                  isSearchBarOpen = !isSearchBarOpen;
+                }),
+            icon: const Icon(Icons.search))
+      ];
+    }
+  }
+
+  Widget _titleBar() {
+    if (isSearchBarOpen) {
+      return Row(
+        children: [
+          IconButton(
+            onPressed: () => setState(() {
+              isSearchBarOpen = !isSearchBarOpen;
+            }),
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(width: 8.0),
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                hintText: 'search in space...',
+                hintStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontStyle: FontStyle.italic,
+                ),
+                border: InputBorder.none,
+                suffixIcon: IconButton(
+                  onPressed: _controller.clear,
+                  icon: const Icon(
+                    Icons.clear,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              cursorColor: Colors.white,
+            ),
+          ),
+          const SizedBox(width: 10.0),
+        ],
+      );
+    } else {
+      return const Text('Astro');
+    }
   }
 }
