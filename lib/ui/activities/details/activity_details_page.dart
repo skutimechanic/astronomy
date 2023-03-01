2c87eac434495e6a6dbebc72fbedca277a97a704
skutimechanic <koszowskimtb@gmail.com>
2023-03-01 13:55:03 +0100

    using content with progress widget

--- lib/ui/activities/details/activity_details_page.dart
+++ lib/ui/activities/details/activity_details_page.dart
@@ -1,5 +1,6 @@
 import 'package:astro_api/astro_api.dart';
 import 'package:astronom/repository/astro_repository.dart';
+import 'package:astronom/ui/widgets/content_with_progress.dart';
 import 'package:astronom/ui/widgets/favorite_icon_button.dart';
 import 'package:astronom/utils/date_time_extension.dart';
 import 'package:astronom/utils/string_extensions.dart';
@@ -50,159 +51,155 @@ class ActivityDetailsPage extends StatelessWidget {
               )
             ],
           ),
-          body: state.status.isLoading
-              ? const Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        child: ListView(
-                          children: [
-                            _showIfValidateDataProvided(
-                              "ID",
-                              activity.id.toString(),
+          body: ContentWithProgress(
+            showProgressInidicator: state.status.isLoading,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        _showIfValidateDataProvided(
+                          "ID",
+                          activity.id.toString(),
+                        ),
+                        _showIfValidateDataProvided(
+                          "CREATION DATE",
+                          activity.creationDate.toFormmatedDateTimeString(),
+                        ),
+                        _showIfValidateDataProvided(
+                          "DATE",
+                          activity.date?.toFormmatedDateTimeString(),
+                        ),
+                        if (activity.links != null &&
+                            activity.links!.isNotEmpty)
+                          _showIfValidateDataProvided(
+                            "LINKS",
+                            activity.links?.toString(),
+                          ),
+                        _showIfValidateDataProvided(
+                          "TITLE",
+                          activity.title,
+                        ),
+                        _showIfValidateDataProvided(
+                          "CONTENT",
+                          activity.content,
+                        ),
+                        _showIfValidateDataProvided(
+                          "LABEL",
+                          activity.label,
+                        ),
+                        _showIfValidateDataProvided(
+                          "PROFILE",
+                          activity.profile,
+                        ),
+                        _showIfValidateDataProvided(
+                          "OBSERVING SITE",
+                          activity.observingSite,
+                        ),
+                        _showIfValidateDataProvided(
+                          "TELESCOPE",
+                          activity.telescope,
+                        ),
+                        _showIfValidateDataProvided(
+                          "INSTRUMENT",
+                          activity.instrument,
+                        ),
+                        if (activity.programme != null) ...[
+                          Column(children: [
+                            const Text(
+                              "PROGRAMME:",
+                              style: TextStyle(fontWeight: FontWeight.bold),
                             ),
                             _showIfValidateDataProvided(
-                              "CREATION DATE",
-                              activity.creationDate.toFormmatedDateTimeString(),
+                              "IDENTIFIER",
+                              activity.programme?.identifier,
                             ),
                             _showIfValidateDataProvided(
-                              "DATE",
-                              activity.date?.toFormmatedDateTimeString(),
+                              "INVESTIGATORS LIST",
+                              activity.programme?.investigatorsList,
                             ),
-                            if (activity.links != null &&
-                                activity.links!.isNotEmpty)
-                              _showIfValidateDataProvided(
-                                "LINKS",
-                                activity.links?.toString(),
-                              ),
+                            _showIfValidateDataProvided(
+                              "INVESTIGATORS",
+                              activity.programme?.investigators.toString(),
+                            ),
                             _showIfValidateDataProvided(
                               "TITLE",
-                              activity.title,
+                              activity.programme?.title,
                             ),
                             _showIfValidateDataProvided(
-                              "CONTENT",
-                              activity.content,
+                              "ABSTRACT",
+                              activity.programme?.abstract_,
                             ),
                             _showIfValidateDataProvided(
-                              "LABEL",
-                              activity.label,
+                              "ABSTRACT ULR",
+                              activity.programme?.abstractUrl,
                             ),
                             _showIfValidateDataProvided(
-                              "PROFILE",
-                              activity.profile,
+                              "FULL DETAILS URL",
+                              activity.programme?.fullDetailsUrl,
                             ),
-                            _showIfValidateDataProvided(
-                              "OBSERVING SITE",
-                              activity.observingSite,
-                            ),
-                            _showIfValidateDataProvided(
-                              "TELESCOPE",
-                              activity.telescope,
-                            ),
-                            _showIfValidateDataProvided(
-                              "INSTRUMENT",
-                              activity.instrument,
-                            ),
-                            if (activity.programme != null) ...[
-                              Column(children: [
-                                const Text(
-                                  "PROGRAMME:",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                                _showIfValidateDataProvided(
-                                  "IDENTIFIER",
-                                  activity.programme?.identifier,
-                                ),
-                                _showIfValidateDataProvided(
-                                  "INVESTIGATORS LIST",
-                                  activity.programme?.investigatorsList,
-                                ),
-                                _showIfValidateDataProvided(
-                                  "INVESTIGATORS",
-                                  activity.programme?.investigators.toString(),
-                                ),
-                                _showIfValidateDataProvided(
-                                  "TITLE",
-                                  activity.programme?.title,
-                                ),
-                                _showIfValidateDataProvided(
-                                  "ABSTRACT",
-                                  activity.programme?.abstract_,
-                                ),
-                                _showIfValidateDataProvided(
-                                  "ABSTRACT ULR",
-                                  activity.programme?.abstractUrl,
-                                ),
-                                _showIfValidateDataProvided(
-                                  "FULL DETAILS URL",
-                                  activity.programme?.fullDetailsUrl,
-                                ),
-                              ])
-                            ],
-                            _showIfValidateDataProvided(
-                              "PROGRAMME TYPE",
-                              activity.programmeType,
-                            ),
-                            _showIfValidateDataProvided(
-                              "TARGET NAME",
-                              activity.targetName,
-                            ),
-                            if (activity.coordinates != null) ...[
-                              Column(children: [
-                                const Text(
-                                  "COORDINATES:",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                                _showIfValidateDataProvided(
-                                  "RIGHT ASCENSION",
-                                  activity.coordinates?.rightAscension
-                                      ?.toString(),
-                                ),
-                                _showIfValidateDataProvided(
-                                  "DECLINATION",
-                                  activity.coordinates?.declination?.toString(),
-                                ),
-                                _showIfValidateDataProvided(
-                                  "EPOCH",
-                                  activity.coordinates?.epoch.toString(),
-                                ),
-                              ])
-                            ],
-                            _showIfValidateDataProvided(
-                              "ORGANISATION",
-                              activity.organisation,
-                            ),
-                            _showIfValidateDataProvided(
-                              "COLLABORATION",
-                              activity.collaboration?.toString(),
-                            ),
-                          ],
+                          ])
+                        ],
+                        _showIfValidateDataProvided(
+                          "PROGRAMME TYPE",
+                          activity.programmeType,
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10.0),
-                        child: ElevatedButton.icon(
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: const Size.fromHeight(50),
-                          ),
-                          onPressed: () =>
-                              context.read<ActivityDetailsBloc>().add(
-                                    FavoriteClick(activity: activity),
-                                  ),
-                          icon: Icon(isFavorite
-                              ? Icons.favorite
-                              : Icons.favorite_border),
-                          label: const Text("Favorite"),
+                        _showIfValidateDataProvided(
+                          "TARGET NAME",
+                          activity.targetName,
                         ),
-                      ),
-                    ],
+                        if (activity.coordinates != null) ...[
+                          Column(children: [
+                            const Text(
+                              "COORDINATES:",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            _showIfValidateDataProvided(
+                              "RIGHT ASCENSION",
+                              activity.coordinates?.rightAscension?.toString(),
+                            ),
+                            _showIfValidateDataProvided(
+                              "DECLINATION",
+                              activity.coordinates?.declination?.toString(),
+                            ),
+                            _showIfValidateDataProvided(
+                              "EPOCH",
+                              activity.coordinates?.epoch.toString(),
+                            ),
+                          ])
+                        ],
+                        _showIfValidateDataProvided(
+                          "ORGANISATION",
+                          activity.organisation,
+                        ),
+                        _showIfValidateDataProvided(
+                          "COLLABORATION",
+                          activity.collaboration?.toString(),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size.fromHeight(50),
+                      ),
+                      onPressed: () => context.read<ActivityDetailsBloc>().add(
+                            FavoriteClick(activity: activity),
+                          ),
+                      icon: Icon(
+                          isFavorite ? Icons.favorite : Icons.favorite_border),
+                      label: const Text("Favorite"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         );
       },
     );
